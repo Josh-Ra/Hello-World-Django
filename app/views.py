82f15c39435b0eabf215ef5c0fad1ab505452076
Veno <joshwad178@outlook.com>
2023-11-01 13:25:55 -0500

    fix

--- app/views.py
+++ app/views.py
@@ -3,5 +3,5 @@ from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 # Create your views here.
 
-def hello_world(request):
-    return HttpResponse("Hello World")
+def hello_world(request:HttpRequest) -> str:
+    return HttpResponse("Hello World!")
